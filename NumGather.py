4ff9a23c1bc826672419918d0ac392e7ac31e414
MR.SH4N <82884347+mr-sh4n@users.noreply.github.com>
2021-04-26 18:16:55 +0530

    Add files via upload
    When you are installing the tools , install it's dependencies 
    ``` pip install phonenumbers ```
    ``` pip install pyfiglet ```
    ``` python NumGather.py ```

---
+++ NumGather.py
@@ -0,0 +1,15 @@
+import pyfiglet
+import phonenumbers
+from phonenumbers import carrier
+from phonenumbers import geocoder
+from pyfiglet import Figlet
+
+pyfiglet.print_figlet("Num Gather",'banner')
+print("This tool was made by H1DD3N_SH4D0W for SecHunt-OS")
+print("\t\t https://github.com/mr-sh4n")
+
+print("If indian Number : +91987456321")
+number = input ("Enter your mobile number : ")
+phone = phonenumbers.parse(number)
+print("The country of this Phone number is : "+geocoder.description_for_number(phone, 'en'))
+print("The ISP of the phone number is : "+carrier.name_for_number(phone,'en'))
