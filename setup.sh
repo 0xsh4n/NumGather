f1e70d800bf27bbf88f13e7cc8aaa99973debfd9
MR.SH4N <82884347+mr-sh4n@users.noreply.github.com>
2021-04-28 16:41:38 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 pip3 install phonenumbers
 pip3 install pyfiglet
-YEL= '\033[1;33m' # Yellow
-LR ='\033[1;31m' # Light Red
-echo -e "${YEL} Now just type ${LR} ./numgather.sh "
 
+printf " Now just type python NumGather.py \n"
+echo "For 1st time we can do it for you "
+
+python NumGather.py
 
